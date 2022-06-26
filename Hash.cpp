b8148bd94f10345b68ec41344a44be704d143285
bcespedes <bcespedes@fi.uba.ar>
2022-06-26 17:33:23 -0300

    Se agrega la clase hash

---
+++ Hash.cpp
@@ -0,0 +1,117 @@
+# include "Hash.h"
+
+
+Hash::Hash() {
+
+    tamanio_tabla_ = CANTIDAD_CLAVES / FACTOR_CARGA;
+
+    tabla_hash_ = new Lista<Escritor*>*[tamanio_tabla_];
+
+    for(int i = 0; i < tamanio_tabla_; i++)
+        tabla_hash_[i] = new Lista<Escritor*>;
+}
+
+
+int Hash::aplicar_division(int clave) {
+
+    return clave % tamanio_tabla_;
+}
+
+
+Lista<Escritor*>* Hash::obtener_lista(int posicion) {
+
+    return tabla_hash_[posicion];
+}
+
+
+int Hash::obtener_posicion_elemento(int clave) {
+
+    int posicion = -1, indice = aplicar_division(clave);
+    bool esta = false;
+
+    Lista<Escritor*>* lista_escritores = obtener_lista(indice);
+    lista_escritores -> inicializar();
+    while(lista_escritores -> hay_siguiente() && !esta) {
+        posicion++;
+        if(lista_escritores -> siguiente() -> obtener_isni() == clave)
+            esta = true;
+    }
+
+    if(!esta)
+        posicion = -1;
+
+    return posicion;
+}
+
+
+bool Hash::vacia() {
+
+    int contador_elementos = 0;
+
+    for(int i = 0; i < tamanio_tabla_; i++) {
+        tabla_hash_[i] -> inicializar();
+        while(tabla_hash_[i] -> hay_siguiente() && contador_elementos == 0)
+            contador_elementos++;
+    }
+
+    return (contador_elementos == 0);
+}
+
+
+bool Hash::esta_el_elemento(int clave) {
+
+    return obtener_posicion_elemento(clave) != -1;
+}
+
+
+void Hash::alta(Escritor* escritor) {
+
+    int clave = escritor -> obtener_isni();
+    int posicion_tabla = aplicar_division(clave);
+
+    tabla_hash_[posicion_tabla] -> alta(escritor, 0);
+}
+
+
+void Hash::baja(int clave) {
+
+    int posicion_tabla = aplicar_division(clave);
+    int posicion = obtener_posicion_elemento(clave);
+
+    tabla_hash_[posicion_tabla] -> baja(posicion);
+}
+
+
+Escritor* Hash::consulta(int clave) {
+
+    int posicion_tabla = aplicar_division(clave);
+    int posicion = obtener_posicion_elemento(clave);
+    Escritor* escritor = tabla_hash_[posicion_tabla] -> consulta(posicion);
+
+    return escritor;
+}
+
+
+void Hash::imprimir_escritores(bool solo_nombres) {
+
+    for(int i = 0; i < tamanio_tabla_; i++) {
+        tabla_hash_[i] -> inicializar();
+        while(tabla_hash_[i] -> hay_siguiente()) {
+            Escritor* escritor = tabla_hash_[i] -> siguiente();
+                cout << "ISNI: [" << escritor -> obtener_isni() << "] - ";
+                if(solo_nombres)
+                    cout << escritor -> obtener_nombre_completo() << endl << endl;
+                else
+                    escritor -> mostrar_escritor();
+        }
+    }
+}
+
+
+Hash::~Hash() {
+
+    for(int i = 0; i < tamanio_tabla_; i++)
+        delete tabla_hash_[i];
+
+    delete[] tabla_hash_;
+}
---
+++ Hash.h
@@ -0,0 +1,59 @@
+# ifndef HASH_H
+# define HASH_H
+
+
+# include "constantes.h"
+# include "Lista.h"
+# include "Escritor.h"
+
+
+class Hash {
+
+private:
+
+    // ATRIBUTOS
+    int tamanio_tabla_;
+    Lista<Escritor*>** tabla_hash_;
+
+    // METODOS
+    int aplicar_division(int clave);
+    Lista<Escritor*>* obtener_lista(int posicion);
+    int obtener_posicion_elemento(int clave);
+
+public:
+
+    // PRE: -
+    // POST: Crea una tabla de hash con punteros a listas vacías.
+    Hash();
+
+    // PRE: -
+    // POST: Devuelve true si la tabla de hash esta completamente vacia, false de lo contrario.
+    bool vacia();
+
+    // PRE: Recibe una clave que debe ser un entero.
+    // POST: Devuelve true si se encuentra la clave en la tabla, false de lo contrario.
+    bool esta_el_elemento(int clave);
+
+    // PRE: Recibe un escritor valido.
+    // POST: Da de alta al escritor en la tabla de hashing.
+    void alta(Escritor* escritor);
+
+    // PRE: Recibe una clave que debe ser un entero. La clave debe existir en la tabla.
+    // POST: Da de baja el elemento con la clave correspondiente en caso de existir.
+    void baja(int clave);
+
+    // PRE: Recibe una clave que debe ser un entero. La clave debe existir en la tabla.
+    // POST: Devuelve el escritor correspondiente a esa clave.
+    Escritor* consulta(int clave);
+
+    // PRE: Recibe un booleano que indica si se deben mostrar o no solo los nombres.
+    // POST: Imprime los escritores que hay en la tabla segun corresponda.
+    void imprimir_escritores(bool solo_nombres);
+
+    // PRE: -
+    // POST: Destruye todas las listas de la tabla y la tabla.
+    ~Hash();
+
+};
+
+# endif
